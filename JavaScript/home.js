80239c47663f2bea7e8de347df69f02909c04620
Abhishek Dam <abhishekdam852@gmail.com>
2020-12-09 21:36:40 +0530

    make

---
+++ JavaScript/home.js
@@ -0,0 +1,50 @@
+console.log("Hello World");
+
+var d = "this is d";
+console.log(d);
+
+var number = 67;
+console.log(number);
+
+
+//inpug vatiable
+var age = prompt("Whait is your age?"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert('yo you');
