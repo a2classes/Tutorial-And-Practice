887fd61c1773a4aad6adda20b9a90903b677dd29
Abhishek Dam <abhishekdam852@gmail.com>
2020-12-10 14:07:10 +0530

    update

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,50 +1,102 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-var d = "this is d";
-console.log(d);
+// var d = "this is d";
+// console.log(d);
 
-var number = 67;
-console.log(number);
+// var number = 67;
+// console.log(number);
 
+// Manupulating DOM using JavaScript
+// inpug vatiable
+// var age = prompt("Whait is your age?");
 
-//inpug vatiable
-var age = prompt("Whait is your age?"); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// document.getElementById("coolExample").innerHTML = age;
 
 // alert('yo you');
+
+// number
+// var num1 = 5;
+// num1++;
+// num1++;
+// num1++;
+// num1 = num1 + num1;
+// console.log(num1%5);
+// console.log(5 * 4);
+// console.log(50 / 5);
+// num1 += 22;
+// console.log(num1);
+// num1 -= 10;
+// console.log(num1);
+
+// function
+
+// function fun() {
+//   console.log("this is a function");
+// }
+
+// fun();
+
+// another function
+
+/*
+this function will take your name and then display your name with a Hello in front of it.
+*/
+
+// function greet() {
+//   var name = prompt("What id your name");
+//   var result = "Hello " + name; //string concatenation
+//   console.log(result);
+// }
+// greet();
+
+// function greet1(yourName) {
+//   var result = "Hello " + yourName; //string concatenation
+//   console.log(result);
+// }
+// var name1 = prompt("What is your name?");
+// greet1(name1);
+
+// function with arguments
+
+// function add(num1, num2) {
+//   var sum = num1 + num2;
+//   console.log(sum);
+// }
+
+// add(5, 4);
+// add("Damn", " you dam");
+
+// while loops
+
+// var number2 = 0;
+
+// while (number2 < 100) {
+//   number2 += 1;
+//   console.log(number2);
+// }
+
+// for loops
+
+// for (let num = 0; num < 200; num++) {
+//   console.log(num);
+// }
+
+// data types
+
+let yourAge = 22;                           //number
+let yourName = 'Abhishek Dam';              //string
+let name = { first: "abhsihek", last: "dam" };//object
+let truth = false;                          //boolean
+let groceries = ['Apple', 'Potato', 'Doritos'];//array
+let random;                                 //undefined
+let nothing = null;                          //value null
+
+//stringts in javascript
+
+let fruit = "apple, banana, orange, berry";
+let newFruit = "banana\napple"              //new line
+console.log(fruit);
+console.log(newFruit);
+
+
+console.log(fruit.length);
